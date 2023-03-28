cb1d208ba0e51aceef72f3737880b9efa2e40054
Ekaterina <katya.leemi@gmail.com>
2023-03-28 19:49:48 +0300

    Added api to cancel and book appointments

--- server/controllers/appointments.js
+++ server/controllers/appointments.js
@@ -9,11 +9,54 @@ const getAppointments = async (req, res) => {
         })
         res.json(list)
     } catch (e) {
+        // add better error handling
         res.json({ msg: 'nothing' })
     }
+}
 
+const bookAppointment = async (req, res) => {
+    try {
+        const { appointmentId } = req.params
+        const { _id: userId } = req
+        const appointment = await Appointment.findById(appointmentId)
+        if(appointment.isBooked){
+            return res.status(400).json({msg: 'This time is already booked'})
+        }
+
+        await Appointment.findByIdAndUpdate(appointmentId, {
+            userId,
+            isBooked: true
+        })
+        
+        res.status(200).json({ msg: 'Booked successfully' })
+    } catch (e) {
+        // add better error handling
+        res.json({ msg: 'nothing' })
+    }
+}
+const cancelAppointment = async (req, res) => {
+    try {
+        const { appointmentId } = req.params
+        const { _id: userId } = req
+        const appointment = await Appointment.findById(appointmentId)
+        
+        if(appointment.userId != userId) {
+            return res.status(400).json({msg: "You cannot cancel other user appointment"})
+        }
+
+        await Appointment.findByIdAndUpdate(appointmentId, {
+            userId: null,
+            isBooked: false
+        })
+        res.status(200).json({ msg: 'Cancelled successfully' })
+    } catch (e) {
+        // add better error handling
+        res.json({ msg: 'nothing' })
+    }
 }
 
 module.exports = {
-    getAppointments
+    getAppointments,
+    bookAppointment,
+    cancelAppointment
 }
--- server/routes/appointments.js
+++ server/routes/appointments.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const {getAppointments} = require('../controllers/appointments');
+const { getAppointments, bookAppointment, cancelAppointment } = require('../controllers/appointments');
+const { verifyToken } = require('../middlewares/verifyToken')
 
 router.get('/:therapistId', getAppointments)
+router.put('/book/:appointmentId', verifyToken, bookAppointment)
+router.put('/cancel/:appointmentId', verifyToken, cancelAppointment)
 
 module.exports = router
